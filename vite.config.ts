e50794eea2feda0466e8e38cc9b92ae0c9b86838
HQ <hector.quijada2121@gmail.com>
2024-03-29 01:38:48 -0400

    Add unocss library and configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { vitePlugin as remix } from "@remix-run/dev";
+import UnoCSS from '@unocss/vite';
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
+    UnoCSS(),
     remix({
       ssr: false,
     }),
